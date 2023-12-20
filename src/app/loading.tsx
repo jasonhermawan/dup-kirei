9f6ffb65ff26421e5d39a1915b673a18b4cb35ca
jasonhermawan <jasonhermawans@gmail.com>
2023-12-20 17:52:02 +0700

    feat: create loader for auth page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,26 +1,38 @@
 "use client";
-import AboutLoader from "@/pageLoader/aboutLoader";
 import CommunityLoader from "@/pageLoader/communityLoader";
-import HomeLoader from "@/pageLoader/homeloader";
+import LoginLoader from "@/pageLoader/loginLoader";
 import PartnershipLoader from "@/pageLoader/partnershipLoader";
 import ServiceLoader from "@/pageLoader/serviceLoader";
-import { Loader } from "@mantine/core";
+import SignupLoader from "@/pageLoader/signupLoader";
+import { Box, Loader } from "@mantine/core";
 import { usePathname } from "next/navigation";
 
 export default function Loading() {
   const pathname = usePathname();
 
   if (pathname === "/") {
-    return <HomeLoader />;
+    return null;
   } else if (pathname === "/about-us") {
-    return <AboutLoader />;
+    return null;
   } else if (pathname === "/our-services") {
     return <ServiceLoader />;
   } else if (pathname === "/partnership") {
     return <PartnershipLoader />;
   } else if (pathname === "/eco-community") {
     return <CommunityLoader />;
+  } else if (pathname === "/login") {
+    return <LoginLoader />;
+  } else if (pathname === "/signup") {
+    return <LoginLoader />;
+  } else if (pathname === "/signup/create-password") {
+    return <LoginLoader />;
+  } else if (pathname === "/signup/otp") {
+    return <SignupLoader />;
   } else {
-    return <Loader color="blue" />;
+    return (
+     <Box w="50%" m="auto" mt="100px">
+      <Loader color="blue" />
+     </Box>
+    );
   }
 }
